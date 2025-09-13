57f78630d17075d62a559b9507fefa37487c6285
gimmeursocks <bobteen1@gmail.com>
2025-09-13 20:56:48 +0300

    Fix region var in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "konecta-week-7-terraform-state-bucket"
     key          = "terraform/prod/terraform.tfstate"
-    region       = var.aws_region
+    region       = "eu-central-1"
     encrypt      = true
     use_lockfile = true
   }
